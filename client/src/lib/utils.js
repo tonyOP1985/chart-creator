c2fc447bf3130432cb46e877809343ba99e9cc0d
Tony Oliver-Paull <toliverpaull@gmail.com>
2020-06-13 18:56:47 -0400

    parseInteger func because airbnb lint rules want a radix.

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Function to help with Airbnb eslint rules wanting a radix.
+ * @param {String} value - Number character that needs to be parsed to an integer.
+ * @return {Number}
+ */
 const parseInteger = value => parseInt(value, 10);
 
 export {
